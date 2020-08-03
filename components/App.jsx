a04d21a9be81db28590de942811999a9334a1b80
Vanessa <vahurtad@ucsc.edu>
2020-08-02 19:16:15 -0700

    primer commit

---
+++ components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Head from "next/head";
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import favicon from "../static/images/favicon/favicon-32x32.png";
+
+const App = ({ children }) => (
+  <React.Fragment>
+    <Head>
+      <title>Laboratorio Smart Machines</title>
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        key="viewport"
+      />
+      {/* <!-- Icono --> */}
+      {/* <link rel="icon" sizes="192x192" href={favicon}></link> */}
+      {/* {Google Fonts} */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600&family=Raleway:wght@400;500&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Head>
+
+    <main>
+      {/* <Header /> */}
+      {children}
+      {/* <Footer /> */}
+    </main>
+  </React.Fragment>
+);
+
+export default App;
---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+// import "../static/styles/theme.css";
+// https://github.com/zeit/next.js/blob/master/errors/css-global.md
+// https://github.com/zeit/next.js/blob/master/errors/app-container-deprecated.md
+
+import App from "next/app";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
