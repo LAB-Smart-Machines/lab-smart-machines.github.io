3c96fbbdf21d691a99e5e0638fddd705daf1a61d
Vanessa <vahurtad@ucsc.edu>
2020-09-21 22:30:48 -0700

    cambios a proyecot

--- components/Readme.jsx
+++ components/Readme.jsx
@@ -3,8 +3,6 @@ import { useQuery } from '@apollo/client';
 import ReactMarkdown from 'react-markdown';
 import { README_GQL } from '../graphql/main';
 
-import Seccion from './Seccion';
-
 const ReadmeComponent = (github) => {
   const nameT = github.github;
 
@@ -14,28 +12,14 @@ const ReadmeComponent = (github) => {
   if (error) return <div>Error, No hay nada</div>;
   return (
     <>
-      <Seccion
-        claseSeccion="header"
-        claseHeading="main-heading"
-        claseSub="main-subtitle"
-        titulo={nameT}
-        subTexto={data ? data.repository.description : ''}
-      />
       {data ? (
-        <div id="projects" className="section grey">
-          <div className="w-container">
-            <div className="divider grey" />
-            <ReactMarkdown
-              source={data.repository.object.text}
-              className="readme"
-              linkTarget="_blank"
-            />
-          </div>
-        </div>
+        <ReactMarkdown
+          source={data.repository.object.text}
+          className="readme"
+          linkTarget="_blank"
+        />
       ) : (
-        <>
-          <div>Buscando...</div>
-        </>
+        <div>Buscando...</div>
       )}
     </>
   );
--- pages/proyecto.jsx
+++ pages/proyecto.jsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import App from '../components/App';
 import Readme from '../components/Readme';
+import Seccion from '../components/Seccion';
 
 const Proyecto = (props) => {
   const { titulo } = props;
   return (
     <App>
-      <Readme github={titulo} />
+      <Seccion
+        claseSeccion="header"
+        claseHeading="main-heading"
+        claseSub="main-subtitle"
+        titulo={titulo}
+      />
+      <div id="projects" className="section grey">
+        <div className="w-container">
+          <div className="divider grey" />
+          <Readme github={titulo} />
+        </div>
+      </div>
     </App>
   );
 };
